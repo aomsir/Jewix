66bccb41027c7a152dcf10994b97a377f35dcae4
aomsir <info@say521.cn>
2022-03-07 20:10:34 +0800

    1

---
+++ src/test/java/com/aomsir/jewix/JewixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aomsir.jewix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JewixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
